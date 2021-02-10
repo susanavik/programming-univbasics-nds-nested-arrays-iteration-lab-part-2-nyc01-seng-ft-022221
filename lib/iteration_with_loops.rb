55465b0965c255125c7ea846e15d5c7c2c5a9454
Susana vik <susana.m.vik@gmail.com>
2021-02-10 23:02:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,10 +4,11 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   min_in_nested_arrays = []
+  min_value = 10
   while element_index < src[row_index].count do
 
-    if src[row_index][element_index] < min_in_nested_arrays
-      min_in_nested_arrays = src[row_index][element_index]
+    if src[row_index][element_index] < min_value
+      min_value = src[row_index][element_index]
     end
     element_index += 1
   end
@@ -17,6 +18,5 @@ while row_index < src.count do
 end
 
 outer_results
-
 
 end
